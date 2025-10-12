f98721ea8af375aada6bc1d94f667a62597f30b3
justinl66 <justinfinity7@gmail.com>
2025-10-12 15:06:35 -0700

    Add SmolVLMModel class definitions to model.h

--- cactus/models/model.h
+++ cactus/models/model.h
@@ -132,5 +132,65 @@ private:
     } weight_nodes_;
 };
 
+class SmolVLMModel : public SmolModel {
+public:
+    struct VLMConfig : public Config {
+        uint32_t vision_embed_dim;
+        uint32_t visual_tokens_per_img;
+        bool     use_pixel_shuffle;
+        uint32_t pixel_shuffle_factor;
+        bool     use_image_tokens;
+        bool     use_layout_tags;
+    };
+
+    SmolVLMModel();
+    explicit SmolVLMModel(const VLMConfig& cfg);
+    ~SmolVLMModel() override = default;
+
+    size_t forward(const std::vector<uint32_t>& tokens,
+                   const std::vector<ImageBatch>& images,
+                   bool use_cache = false);
+
+protected:
+    size_t build_vision_embeddings(CactusGraph* gb,
+                                   const std::vector<ImageBatch>& images,
+                                   ComputeBackend backend);
+
+    size_t build_combined_input(CactusGraph* gb,
+                                size_t vision_embeds,
+                                const std::vector<uint32_t>& tokens,
+                                ComputeBackend backend,
+                                uint32_t& prefix_len);
+
+    void load_weights_to_graph(CactusGraph* gb) override;
+
+private:
+    struct WeightNodeIDs {
+        size_t vision_proj_weight;
+        size_t vision_proj_bias;
+        size_t image_start_embedding;
+        size_t image_end_embedding;
+        size_t row_tag_embedding;
+        size_t col_tag_embedding;
+
+        struct VisionLayerWeights {
+            size_t attn_q_weight;
+            size_t attn_k_weight;
+            size_t attn_v_weight;
+            size_t attn_output_weight;
+            size_t layer_norm1_weight;
+            size_t layer_norm1_bias;
+            size_t layer_norm2_weight;
+            size_t layer_norm2_bias;
+            size_t mlp_fc1_weight;
+            size_t mlp_fc1_bias;
+            size_t mlp_fc2_weight;
+            size_t mlp_fc2_bias;
+        };
+
+        std::vector<VisionLayerWeights> vision_layers;
+    } weight_nodes_;
+};
+
 }
 }
